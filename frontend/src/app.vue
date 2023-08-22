650ad766936a762c8d13722153f23c344924cfab
Matt <mattclegg93@gmail.com>
2023-08-22 21:21:10 +0100

    feat: added back in call

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <strong>Hello world</strong>
+    <strong>News</strong>
+    <pre>{{ newsItems }}</pre>
   </div>
 </template>
+
+<script setup lang="ts">
+const {getItems} = useDirectusItems();
+
+const {
+  data: newsItems,
+} = await useAsyncData("news", () => {
+  return getItems({
+    collection: "news"
+  })
+})
+</script>
