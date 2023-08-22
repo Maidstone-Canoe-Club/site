76ac69a1323853a4a77fae9ce43d147df4f50cb8
Matt <mattclegg93@gmail.com>
2023-08-22 21:16:17 +0100

    chore: remove commented out code

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,18 +1,5 @@
 <template>
   <div>
     <strong>Hello world</strong>
-    <pre>{{ // newsItems }}</pre>
   </div>
 </template>
-
-<script setup lang="ts">
-// const {getItems} = useDirectusItems();
-//
-// const {
-//   data: newsItems,
-// } = await useAsyncData("news", () => {
-//   return getItems({
-//     collection: "news"
-//   })
-// })
-</script>
