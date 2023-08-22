8eeb7cefc7f62fc9e4b6fb3b57510d75445e3551
Matt <mattclegg93@gmail.com>
2023-08-22 07:31:47 +0100

    feat: added snapshot and extension

---
+++ directus/extensions/directus-extension-slugify/src/index.ts
@@ -0,0 +1,36 @@
+import { defineHook } from '@directus/extensions-sdk';
+
+const events = ["items.create", "items.update"];
+const collections = ["content", "news"];
+
+
+type Content = {
+	title?: string,
+	slug: string
+}
+export default defineHook(({ filter }) => {
+
+	filter("*.*", (payload, meta) => {
+		if(events.includes(meta.event) && collections.includes(meta.collection)){
+			const item = payload as Content;
+
+			if(item.title){
+				item.slug = slugify(item.title);
+			}
+
+			return item;
+		}
+
+		return payload;
+	});
+});
+
+const slugify = (input: string) => {
+	return input
+		.normalize('NFD')
+		.replace(/[\u0300-\u036f]/g, '')
+		.toLocaleLowerCase()
+		.trim()
+		.replace(/[^a-z0-9 ]/g, '')
+		.replace(/\s+/g, '-');
+}
