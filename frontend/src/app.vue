767a9bbb215cd1e5a7b57f387a2f3013547cc4c7
Matt <mattclegg93@gmail.com>
2023-08-22 21:15:00 +0100

    chore: removed directus call

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <strong>News</strong>
-    <pre>{{ newsItems }}</pre>
+    <strong>Hello world</strong>
+    <pre>{{ // newsItems }}</pre>
   </div>
 </template>
 
 <script setup lang="ts">
-const {getItems} = useDirectusItems();
-
-const {
-  data: newsItems,
-} = await useAsyncData("news", () => {
-  return getItems({
-    collection: "news"
-  })
-})
+// const {getItems} = useDirectusItems();
+//
+// const {
+//   data: newsItems,
+// } = await useAsyncData("news", () => {
+//   return getItems({
+//     collection: "news"
+//   })
+// })
 </script>
