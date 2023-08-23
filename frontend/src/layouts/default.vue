a78fa92ec5c20388ad6ee666a406a5153a6a2293
Matt <mattclegg93@gmail.com>
2023-08-23 19:38:52 +0100

    feat: added basic login flow

---
+++ frontend/src/layouts/default.vue
@@ -0,0 +1,38 @@
+﻿<template>
+  <div>
+    <template
+      v-if="user">
+      <nuxt-link
+        to="/profile">
+        Profile
+      </nuxt-link>
+      <button type="button" @click="onLogout">
+        Logout
+      </button>
+    </template>
+    <nuxt-link
+      v-else
+      to="/login">
+      Login
+    </nuxt-link>
+
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useDirectusUser, useDirectusAuth, navigateTo } from "#imports";
+
+const user = useDirectusUser();
+const { logout } = useDirectusAuth();
+
+async function onLogout () {
+  await logout();
+  await navigateTo("/");
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ frontend/src/pages/profile.vue
@@ -0,0 +1,20 @@
+﻿<template>
+  <div>
+    <strong>profile</strong>
+    <pre>{{ user }}</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useDirectusUser, definePageMeta } from "#imports";
+
+definePageMeta({
+  middleware: ["auth"]
+});
+
+const user = useDirectusUser();
+</script>
+
+<style scoped lang="scss">
+
+</style>
