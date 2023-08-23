8c28879bc324e649ac1c6a7aa66673ab1225de28
Matt <mattclegg93@gmail.com>
2023-08-23 07:09:54 +0100

    feat: added index

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,32 +1,5 @@
 <template>
   <div>
-    <strong>News</strong>
-    <pre v-if="newsItems">{{ newsItems }}</pre>
-    <pre v-else>No news</pre>
+    <nuxt-page />
   </div>
 </template>
-
-<script setup lang="ts">
-// const {getItems} = useDirectusItems();
-
-const newsItems = loadNews();
-
-function loadNews () {
-  console.log("loading news");
-  return null;
-//   try{
-//     const {data: newsItem}= await useAsyncData("news", () => {
-//       return getItems({
-//         collection: "news"
-//       })
-//     })
-//
-//     return newsItem;
-//
-//   }catch(e){
-//     console.log("didn't work", e);
-//     return null;
-//   }
-}
-
-</script>
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,32 @@
+﻿<template>
+  <div>
+    <strong>News</strong>
+    <pre v-if="newsItems">{{ newsItems }}</pre>
+    <pre v-else>No news</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { getItems } = useDirectusItems();
+
+const newsItems = await loadNews();
+
+async function loadNews () {
+  try {
+    const { data: newsItem } = await useAsyncData("news", () => {
+      return getItems({
+        collection: "news"
+      });
+    });
+
+    return newsItem;
+  } catch (e) {
+    console.log("didn't work", e);
+    return null;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
