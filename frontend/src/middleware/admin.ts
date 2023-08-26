1598ffcc69df80ee55c32fb7811635705b80d09d
Matt <mattclegg93@gmail.com>
2023-08-26 06:34:59 +0100

    feat: started on user invite frontend and added admin check middleware

---
+++ frontend/src/middleware/admin.ts
@@ -0,0 +1,8 @@
+﻿export default defineNuxtRouteMiddleware(() => {
+  const user = useDirectusUser();
+  if (user.value.role !== "b4a0ccc9-6378-4b29-a3d5-dfb065b2ff42") {
+    return createError({
+      statusCode: 401
+    });
+  }
+});
