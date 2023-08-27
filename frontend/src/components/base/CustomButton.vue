0ca560bcdaa9ea995f621b9456f09c72f27130d5
Matt <mattclegg93@gmail.com>
2023-08-27 17:44:01 +0100

    fix: import tailwind components in main css

--- frontend/src/components/base/CustomButton.vue
+++ frontend/src/components/base/CustomButton.vue
@@ -50,15 +50,3 @@ async function onClick () {
 }
 
 </script>
-
-<style lang="css" scoped>
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-</style>
