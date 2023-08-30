d4dd026025f27e31066f6e05c9f22cbd068ddf7b
Matt <mattclegg93@gmail.com>
2023-08-30 07:03:18 +0100

    feat: added sentry to directus

---
+++ directus/extensions/directus-extension-sentry/src/index.ts
@@ -0,0 +1,22 @@
+﻿import * as Sentry from "@sentry/node";
+
+export default defineHook(({init}, {env}) => {
+    const {SENTRY_DSN} = env;
+
+    if (SENTRY_DSN) {
+        Sentry.init({
+            dsn: SENTRY_DSN,
+			tracesSampleRate: 1.0
+        });
+
+        init('routes.before', ({app}) => {
+            app.use(Sentry.Handlers.requestHandler());
+            console.log('-- Sentry Request Handler Added --');
+        });
+
+        init('routes.custom.after', ({app}) => {
+            app.use(Sentry.Handlers.errorHandler());
+            console.log('-- Sentry Error Handler Added --');
+        });
+    }
+});
