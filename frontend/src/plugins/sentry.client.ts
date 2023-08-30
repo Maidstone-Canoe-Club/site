0bc4a3b7c7c6f1fdd4fc586931c7e42099c97e22
Matt <mattclegg93@gmail.com>
2023-08-30 06:42:34 +0100

    chore: lowered trace sample rate and added upload source map to dockerfile

--- frontend/src/plugins/sentry.client.ts
+++ frontend/src/plugins/sentry.client.ts
@@ -22,7 +22,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 1.0
+    tracesSampleRate: 0.2
   });
 
   return {
