48b47ca0cf67909626fccb254c376ebbacbb2c7e
Matt <34515355+matt-clegg@users.noreply.github.com>
2023-09-01 06:39:17 +0100

    feat: added content pages (#21)

---
+++ frontend/src/utils/redirects.ts
@@ -0,0 +1,13 @@
+﻿export async function redirectToSlug (item: any) {
+  const route = useRoute();
+
+  if (!route.params.slug && item.slug) {
+    let redirect = route.path;
+    if (!redirect.endsWith("/")) {
+      redirect += "/";
+    }
+
+    redirect += item.slug;
+    await navigateTo(redirect);
+  }
+}
