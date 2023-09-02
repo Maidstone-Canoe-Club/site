4b99da7579ba906ba2b74b9c8cbfe06f19f28cbe
Matt <mattclegg93@gmail.com>
2023-09-02 10:03:54 +0100

    feat: added eslint to extension and added mailing lists

---
+++ directus/extensions/directus-extension-mcc/src/types.d.ts
@@ -0,0 +1,74 @@
+﻿export type FullAddress = {
+    Email: string,
+    Name: string,
+    MailboxHash: string
+}
+
+export type Header = {
+    Name: string,
+    Value: string
+}
+
+export type Attachment = {
+    Name: string,
+    Content: string,
+    ContentType: string,
+    ContentLength: number
+}
+
+export type OutboundEmail = {
+    From: string,
+    To: string,
+    Cc?: string,
+    Bcc?: string,
+    Subject?: string,
+    Tag?: string,
+    HtmlBody?: string,
+    TextBody?: string
+    ReplyTo?: string,
+    Headers?: string[],
+    TrackOpens?: boolean,
+    TrackLinks?: string,
+    Metadata?: object,
+    Attachments?: Attachment[],
+    MessageStream?: string
+}
+
+export type InboundEmail = {
+    FromName: string,
+    MessageStream: string,
+    From: string,
+    FromFull: FullAddress,
+    To: string,
+    ToFull: FullAddress[],
+    Cc: string,
+    CcFull: FullAddress[],
+    Bcc: string,
+    BccFull: FullAddress[],
+    OriginalRecipient: string,
+    Subject: string,
+    MessageID: string,
+    MailboxHash: string,
+    Date: string,
+    TextBody: string,
+    HtmlBody: string,
+    StrippedTextReply: string,
+    Tag: string;
+    Headers: Header[],
+    Attachments: Attachment[]
+}
+
+export type Subscriber = {
+    mailing_list: number,
+    user: {
+        email: string,
+        first_name: string,
+        last_name: string
+    }
+}
+
+export type MailingList = {
+    id: number,
+    name: string,
+    email_name: string
+}
