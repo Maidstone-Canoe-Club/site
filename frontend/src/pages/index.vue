fa84bf24fefca7c685b69f703ada4bbc3d4ebebd
Matt <34515355+matt-clegg@users.noreply.github.com>
2023-09-02 14:14:30 +0100

    feat: added sentry to directus (#40)

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,10 +1,17 @@
 ﻿<template>
   <div>
     <strong>index</strong>
+    <button @click="throwAnError">
+      Throw Error
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
+
+function throwAnError () {
+  throw new Error("I was thrown!");
+}
 
 </script>
 
