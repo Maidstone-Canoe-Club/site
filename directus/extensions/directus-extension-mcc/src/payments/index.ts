5225b8924e7eb07fb3520f4d6cc6769cdfc0a578
Matt <34515355+matt-clegg@users.noreply.github.com>
2023-09-15 16:39:34 +0100

    fix: added nuxt endpoint to handle stripe webhook events (#63)

---
+++ directus/extensions/directus-extension-mcc/src/payments/index.ts
@@ -0,0 +1,58 @@
+import {defineEndpoint} from "@directus/extensions-sdk";
+
+export default defineEndpoint((router, {services}) => {
+  const {ItemsService} = services;
+  const adminAccountability = {
+    admin: true
+  };
+
+  router.get("/handle/event", async (req, res) => {
+
+    try {
+      const sessionWithLineItems = req.body.sessionWithLineItems;
+      const metadata = sessionWithLineItems.metadata;
+
+      if(!sessionWithLineItems){
+        return res.status(400).send("missing data");
+      }
+
+      // TODO: payments will be for different things like memberships, boat storage, keys, ect, need to handle for them
+
+      const bookingService = new ItemsService("event_bookings", {
+        schema: req.schema,
+        accountability: adminAccountability
+      });
+
+      // TODO: status paid?
+      await bookingService.createOne({
+        user: metadata.user_id,
+        event: metadata.event_id,
+        instance: metadata.event_instance
+      });
+
+      const ordersService = new ItemsService("orders", {
+        schema: req.schema,
+        accountability: adminAccountability
+      });
+
+      await ordersService.createOne({
+        user: metadata.user_id,
+        amount: sessionWithLineItems.amount_total,
+        customer_id: sessionWithLineItems.customer,
+        description: `${metadata.event_name} - ${metadata.date}`,
+        payment_intent: sessionWithLineItems.payment_intent,
+        metadata: JSON.stringify({
+          event_id: metadata.event_id,
+          instance: metadata.event_instance,
+        })
+      });
+
+      // TODO: send payment complete email to user
+
+      return res.send("ok");
+    }catch(e){
+      console.error("error handling event payment", e);
+      return res.status(500).send("error handling event payment");
+    }
+  });
+});
--- directus/extensions/directus-extension-mcc/src/user-created-hook/index.ts
+++ directus/extensions/directus-extension-mcc/src/user-created-hook/index.ts
@@ -1,16 +1,16 @@
 ﻿import {defineHook} from "@directus/extensions-sdk";
 import sendConfirmAccountEmail from "../send-email";
-import express from "express";
+// import express from "express";
 export default defineHook(({filter, init}, {services}) => {
 
   // TODO: MOVE THIS TO IT'S OWN EXTENSION
-  init("middlewares.before", async ({app}) => {
-    app.use(express.json({
-      verify: (req, _, buf) => {
-        req.rawBody = buf.toString();
-      }
-    }));
-  } );
+  // init("middlewares.before", async ({app}) => {
+  //   app.use(express.json({
+  //     verify: (req, _, buf) => {
+  //       req.rawBody = buf.toString();
+  //     }
+  //   }));
+  // } );
 
   filter("users.create", async (input, _, {schema, database}) => {
     try {
