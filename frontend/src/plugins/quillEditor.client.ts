09f903c3cc210cd78206bdeb97293c840be326be
Matt <mattclegg93@gmail.com>
2023-09-18 16:24:59 +0100

    perf: lazy load the quill editor in the wysiwyg component

--- frontend/src/plugins/quillEditor.client.ts
+++
@@ -1,7 +0,0 @@
-﻿import { QuillEditor } from "@vueup/vue-quill";
-import "@vueup/vue-quill/dist/vue-quill.snow.css";
-
-export default defineNuxtPlugin(() => {
-  const { vueApp } = useNuxtApp();
-  vueApp.component("QuillEditor", QuillEditor);
-});
