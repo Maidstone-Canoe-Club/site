0bd1e43269dd8cc89a70b28c9ec6d8cfab1c0e4d
Matt <mattclegg93@gmail.com>
2023-12-11 19:54:26 +0000

    fix: conditionally show the cancel booking button

--- frontend/src/components/events/EventAttendees.vue
+++ frontend/src/components/events/EventAttendees.vue
@@ -29,7 +29,7 @@
           View
         </button>
         <button
-          v-else
+          v-else-if="canCancelBooking(booking.user)"
           type="button"
           class="rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
           @click="cancelBooking(booking.user)">
