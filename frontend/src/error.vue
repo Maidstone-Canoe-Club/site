1d4d35eb73aa68a6b7db33e943966a7b4119493f
Matt <mattclegg93@gmail.com>
2024-02-29 18:23:29 +0000

    feat: error page refactor

--- frontend/src/error.vue
+++ frontend/src/error.vue
@@ -2,20 +2,20 @@
   <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
     <div class="text-center">
       <p class="text-base font-semibold text-indigo-600">
-        {{ statusCode }}
+        {{ error.statusCode }}
       </p>
       <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-        {{ title }}
+        Something went wrong
       </h1>
       <p class="mt-6 text-base leading-7 text-gray-700">
-        {{ description }}
+        {{ message }}
       </p>
       <div class="mt-5">
-        <button
-          class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          @click="router.back()">
-          Go back
-        </button>
+        <a-button
+          :disabled="state === 'reloading'"
+          :action="reload">
+          {{ state === "reloading" ? "Reloading" : "Reload" }}
+        </a-button>
       </div>
     </div>
   </main>
@@ -26,37 +26,64 @@ import type { NuxtError } from "#app";
 
 const router = useRouter();
 
-const props = defineProps<{
-  error: NuxtError
+const { error } = defineProps<{
+  error: Partial<NuxtError>
 }>();
 
-const statusCode = computed(() => {
-  return props.error.statusCode;
-});
+const errorCodes: Record<number, string> = {
+  404: "Page not found"
+};
 
-const is404 = computed(() => {
-  return statusCode.value === 404;
-});
+if (process.dev) {
+  console.error(error);
+}
 
-const title = computed(() => {
-  if (is404.value) {
-    return "Page not found";
+const defaultMessage = "Something went wrong";
+
+const message = error.message ?? errorCodes[error.statusCode!] ?? defaultMessage;
+
+const state = ref<"error" | "reloading">("error");
+
+async function reload () {
+  state.value = "reloading";
+
+  try {
+    await clearError({
+      redirect: "/"
+    });
+  } catch (err) {
+    console.error(err);
+    state.value = "error";
   }
+}
 
-  return "An error occured";
-});
-
-const description = computed(() => {
-  if (is404.value) {
-    return "We couldn't find that page";
-  }
-
-  if (props.error.statusMessage) {
-    return props.error.statusMessage;
-  }
-
-  return "There was a problem rendering that page";
-});
+// const statusCode = computed(() => {
+//   return props.error.statusCode;
+// });
+//
+// const is404 = computed(() => {
+//   return statusCode.value === 404;
+// });
+//
+// const title = computed(() => {
+//   if (is404.value) {
+//     return "Page not found";
+//   }
+//
+//   return "An error occured";
+// });
+//
+// const description = computed(() => {
+//   if (is404.value) {
+//     return "We couldn't find that page";
+//   }
+//
+//   if (props.error.statusMessage) {
+//     return props.error.statusMessage;
+//   }
+//
+//   return "There was a problem rendering that page";
+// });
 
 </script>
 
