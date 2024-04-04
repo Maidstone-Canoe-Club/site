76098621094e610425f12b39ce07c5e9ed2b75dd
Matt <34515355+matt-clegg@users.noreply.github.com>
2024-04-04 07:49:11 +0100

    Feat/checkin (#209)
    * feat: added checkin demo
    
    * feat: improvements to checkin scanner

---
+++ frontend/src/utils/strings.ts
@@ -0,0 +1,9 @@
+﻿export function capitalize (input: string) {
+  return input[0].toUpperCase() + input.slice(1);
+}
+
+export function clickOrTap (shouldCapitalize?: boolean) {
+  const { isMobileOrTablet } = useDevice();
+  const result = isMobileOrTablet ? "tap" : "click";
+  return shouldCapitalize ? capitalize(result) : result;
+}
