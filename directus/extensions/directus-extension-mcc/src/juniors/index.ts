4c15f179d4f35116be4dfe6184b184ceb4de910a
Matt <mattclegg93@gmail.com>
2024-04-08 21:30:13 +0100

    fix: property null check

--- directus/extensions/directus-extension-mcc/src/juniors/index.ts
+++ directus/extensions/directus-extension-mcc/src/juniors/index.ts
@@ -54,7 +54,7 @@ export default defineEndpoint((router, {services, database}) => {
         bc_number: juniorUser.bc_number
       });
 
-      if (juniorUser.medicalInformation.hasData) {
+      if (juniorUser.medicalInformation?.hasData) {
         const medicalInfoService = new ItemsService("medical_info", {
           knex: database,
           schema: req.schema,
@@ -108,7 +108,7 @@ export default defineEndpoint((router, {services, database}) => {
         email_confirmed: true
       });
 
-      if (juniorUser.medicalInformation.hasData) {
+      if (juniorUser.medicalInformation?.hasData) {
         const medicalInfoService = new ItemsService("medical_info", {
           knex: database,
           schema: req.schema,
