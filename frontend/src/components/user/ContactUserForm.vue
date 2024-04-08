ead68291cd26f45b4f98bfff02641ec53e5e7215
Matt <mattclegg93@gmail.com>
2024-04-08 13:27:28 +0100

    feat: improvements to coach contact form

--- frontend/src/components/user/ContactUserForm.vue
+++ frontend/src/components/user/ContactUserForm.vue
@@ -4,8 +4,9 @@ import type { Validation } from "@vuelidate/core";
 import { email as emailValidator, maxLength, required } from "@vuelidate/validators";
 import { ref, type Ref } from "vue";
 import { ofetch } from "ofetch";
+import { CheckCircleIcon } from "@heroicons/vue/24/outline";
 
-const emits = defineEmits(["sent"]);
+const emits = defineEmits(["sent", "close"]);
 
 const props = defineProps<{
   userId: string
@@ -56,59 +57,90 @@ async function onSubmit () {
   }
 }
 
+function onClose () {
+  emits("close");
+}
+
 </script>
 
 <template>
-  <form
-    method="post"
-    @submit.prevent>
-    <nuxt-turnstile
-      v-model="formData.token"
-      :options="turnstileOptions" />
-    <input
-      id="user-id"
-      type="hidden"
-      name="userId"
-      :value="userId">
-    <div class="space-y-6">
-      <input-field
-        id="from-name"
-        v-model="formData.fromName"
-        name="fromName"
-        label="From name"
-        :v="v$.fromName" />
-      <input-field
-        id="from-email"
-        v-model="formData.fromEmail"
-        type="email"
-        name="fromEmail"
-        label="From email"
-        :v="v$.fromEmail" />
-
-      <input-field
-        id="subject"
-        v-model="formData.subject"
-        name="subject"
-        label="Subject"
-        :v="v$.subject" />
-
+  <div>
+    <div
+      v-if="success"
+      class="flex items-center justify-center py-6 sm:py-12 flex-col gap-2">
+      <CheckCircleIcon
+        class="w-24 h-24 mb-1 text-lime-600" />
+      <h2 class="font-bold text-2xl">
+        Message sent!
+      </h2>
       <div>
-        <input-text-area
-          id="message"
-          v-model="formData.message"
-          name="mame"
-          label="Message"
-          :v="v$.message" />
-        <span class="text-sm text-gray-500">Max 1000 characters</span>
+        <a-button
+          :action="onClose"
+          variant="outline"
+          hide-loader>
+          Okay
+        </a-button>
       </div>
-
-      <a-button
-        type="submit"
-        :action="onSubmit">
-        Send
-      </a-button>
     </div>
-  </form>
+    <form
+      v-else
+      method="post"
+      @submit.prevent>
+      <nuxt-turnstile
+        v-model="formData.token"
+        :options="turnstileOptions" />
+      <input
+        id="user-id"
+        type="hidden"
+        name="userId"
+        :value="userId">
+      <div class="space-y-6">
+        <input-field
+          id="from-name"
+          v-model="formData.fromName"
+          name="fromName"
+          label="From name"
+          :v="v$.fromName" />
+        <input-field
+          id="from-email"
+          v-model="formData.fromEmail"
+          type="email"
+          name="fromEmail"
+          label="From email"
+          :v="v$.fromEmail" />
+
+        <input-field
+          id="subject"
+          v-model="formData.subject"
+          name="subject"
+          label="Subject"
+          :v="v$.subject" />
+
+        <div>
+          <input-text-area
+            id="message"
+            v-model="formData.message"
+            name="mame"
+            label="Message"
+            :v="v$.message" />
+          <span class="text-sm text-gray-500">Max 1000 characters</span>
+        </div>
+
+        <div class="flex w-full justify-end items-center gap-2">
+          <a-button
+            variant="outline"
+            :action="onClose">
+            Cancel
+          </a-button>
+          <a-button
+            type="submit"
+            :action="onSubmit">
+            Send
+          </a-button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <style scoped lang="scss">
