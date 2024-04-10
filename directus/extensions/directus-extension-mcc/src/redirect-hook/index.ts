3f9c2d431f2781969d481ccf235b7af78f3cc1a2
Matt <mattclegg93@gmail.com>
2024-04-10 07:54:16 +0100

    feat: renaming news posts now creates redirect

--- directus/extensions/directus-extension-mcc/src/redirect-hook/index.ts
+++ directus/extensions/directus-extension-mcc/src/redirect-hook/index.ts
@@ -1,64 +1,12 @@
 import {defineHook} from "@directus/extensions-sdk";
 
 export default defineHook(({filter}, {services}) => {
-
-  const {ItemsService} = services;
-  const accountability = {
-    admin: true
-  };
-
   filter("items.update", async (payload: any, meta: any, context: any) => {
     try {
-      if (meta.collection !== "events") {
-        return;
-      }
-
-      if (payload.title) {
-        const newSlug = slugify(payload.title);
-        console.log("new slug", newSlug);
-
-        const eventsService = new ItemsService("events", {schema: context.schema, accountability});
-        const redirectService = new ItemsService("redirects", {schema: context.schema, accountability});
-
-        const event = await eventsService.readOne(meta.keys[0], {
-          fields: ["title", "id"]
-        });
-
-        const oldSlug = slugify(event.title);
-
-        const oldUrl = `/events/${event.id}/${oldSlug}`;
-        const newUrl = `/events/${event.id}/${newSlug}`;
-
-        if (oldUrl !== newUrl) {
-
-          const existing = await redirectService.readByQuery({
-            fields: ["url_old", "url_new", "id"],
-            filter: {
-              _and: [
-                {
-                  url_old: {_eq: newUrl},
-                },
-                {
-                  url_new: {_eq: oldUrl}
-                }
-              ]
-            }
-          });
-
-          if (existing && existing.length) {
-            console.log(`found ${existing.length} opposite redirect(s) to new redirect, deleting...`);
-            await redirectService.deleteMany(existing.map((e: any) => e.id));
-          }
-
-          await redirectService.createOne({
-            url_old: oldUrl,
-            url_new: newUrl,
-            response_code: "301"
-          });
-          console.log(`created new redirect: ${oldUrl} -> ${newUrl}`);
-        } else {
-          console.error(`Cannot create redirect with same URLs: ${oldSlug}`);
-        }
+      if (meta.collection === "events") {
+        await handleEventRename(payload, meta, services, context);
+      } else if (meta.collection === "news") {
+        await handleNewsRename(payload, meta, services, context);
       }
     } catch (err) {
       console.error("error in event update hook", err);
@@ -67,6 +15,73 @@ export default defineHook(({filter}, {services}) => {
     return payload;
   });
 });
+
+async function handleNewsRename(payload: any, meta: any, services: any, context: any) {
+  if (!payload.title) {
+    return;
+  }
+
+  await handlePayloadTitleChange(payload, "news", meta, services, context);
+}
+
+async function handleEventRename(payload: any, meta: any, services: any, context: any) {
+  if (!payload.title) {
+    return;
+  }
+  await handlePayloadTitleChange(payload, "events", meta, services, context);
+}
+
+async function handlePayloadTitleChange(payload: any, collection: string,  meta: any, services: any, context: any){
+  const {ItemsService} = services;
+  const accountability = {
+    admin: true
+  };
+
+  const newSlug = slugify(payload.title);
+  console.log(`new ${collection} slug`, newSlug);
+
+  const itemsService = new ItemsService(collection, {schema: context.schema, accountability});
+  const redirectService = new ItemsService("redirects", {schema: context.schema, accountability});
+
+  const item = await itemsService.readOne(meta.keys[0], {
+    fields: ["title", "id"]
+  });
+
+  const oldSlug = slugify(item.title);
+
+  const oldUrl = `/${collection}/${item.id}/${oldSlug}`;
+  const newUrl = `/${collection}/${item.id}/${newSlug}`;
+
+  if (oldUrl !== newUrl) {
+    const existing = await redirectService.readByQuery({
+      fields: ["url_old", "url_new", "id"],
+      filter: {
+        _and: [
+          {
+            url_old: {_eq: newUrl},
+          },
+          {
+            url_new: {_eq: oldUrl}
+          }
+        ]
+      }
+    });
+
+    if (existing && existing.length) {
+      console.log(`found ${existing.length} opposite redirect(s) to new redirect, deleting...`);
+      await redirectService.deleteMany(existing.map((e: any) => e.id));
+    }
+
+    await redirectService.createOne({
+      url_old: oldUrl,
+      url_new: newUrl,
+      response_code: "301"
+    });
+    console.log(`created new ${collection} redirect: ${oldUrl} -> ${newUrl}`);
+  } else {
+    console.error(`Cannot create redirect with same URLs: ${oldSlug}`);
+  }
+}
 
 const slugify = (input: string) => {
   return input
