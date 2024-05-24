dcbd1585faf084057326d5d0109962ce8a97d507
Matt <34515355+matt-clegg@users.noreply.github.com>
2024-05-24 09:18:35 +0100

    fix: fixed error thrown when accessing localStorage (#322)

---
+++ frontend/src/composables/useLocalStorage.ts
@@ -0,0 +1,29 @@
+﻿export function useLocalStorage() {
+  function getItem(name: string) {
+    if (import.meta.client) {
+      try {
+        return window.localStorage.getItem(name);
+      } catch {
+        console.warn("localStorage is not available in this environment");
+      }
+    } else {
+      return undefined;
+    }
+  }
+
+  function setItem(name: string, value: string) {
+    if (import.meta.client) {
+      try {
+        window.localStorage.setItem(name, value);
+        return true;
+      } catch {
+        console.warn("localStorage is not available in this environment");
+      }
+    }
+  }
+
+  return {
+    getItem,
+    setItem
+  }
+}
