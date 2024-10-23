bbc0a658f08c3954d747a383e21e01f6cbe2c333
Matt <34515355+matt-clegg@users.noreply.github.com>
2024-10-23 06:53:15 +0100

    refactor: revert sentry (#383)

--- frontend/sentry.server.config.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from "@sentry/nuxt";
-
-if(process.env.SENTRY_DSN) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    environment: process.env.SENTRY_ENVIRONMENT,
-    tracesSampleRate: 1.0,
-  });
-}
