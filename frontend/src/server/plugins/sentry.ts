bf2c6739f9d436f3c752db577c685c27c93236c5
Matt <34515355+matt-clegg@users.noreply.github.com>
2024-10-23 07:08:43 +0100

    fix: sentry node version (#385)

--- frontend/src/server/plugins/sentry.ts
+++ frontend/src/server/plugins/sentry.ts
@@ -1,7 +1,5 @@
 import * as Sentry from "@sentry/node";
-import {
-  nodeProfilingIntegration
-} from "@sentry/profiling-node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node"
 import { H3Error } from "h3";
 
 export default defineNitroPlugin((nitroApp) => {
