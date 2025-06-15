8bad049f63d4a9327eaaaf8890c53feac025d2a6
Matt <34515355+matt-clegg@users.noreply.github.com>
2025-06-15 16:28:38 +0100

    fix: fixed console log in user update (#403)

--- directus/extensions/directus-extension-mcc/src/user-admin/index.ts
+++ directus/extensions/directus-extension-mcc/src/user-admin/index.ts
@@ -2,7 +2,8 @@ import {defineEndpoint} from "@directus/extensions-sdk";
 import {update} from "./update";
 
 export default defineEndpoint((router, {services, database}) => {
-    router.post("/update-user", async (req: any, res: any) => {
-        return await update(req, res, services, database);
-    });
+  router.post("/update-user", async (req: any, res: any) => {
+    return await update(req, res, services, database);
+  });
 });
+
