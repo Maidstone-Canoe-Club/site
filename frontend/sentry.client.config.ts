3257625b0ac94978ddf5f546ade80f171576007b
Matt <mattclegg93@gmail.com>
2025-09-17 20:10:45 +0100

    fix: rollback sentry changes

--- frontend/sentry.client.config.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from "@sentry/nuxt";
-import { useRuntimeConfig } from "#imports";
-
-const config = useRuntimeConfig();
-
-Sentry.init({
-  dsn: config.public.sentry.dsn,
-  environment: config.public.sentry.environment
-});
